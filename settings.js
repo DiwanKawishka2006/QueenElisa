b0fb7ca5a4e954fac20e9a466922ce489993c42f
DiwanKawishka2006 <diwankawishka2006@gmail.com>
2024-10-03 11:11:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
+  SESSION_ID: 'QUEENELISA;;;==QU1J0QpFWWqdjd5EVNshVWllGdnVEa4ZWMNFVcqhTTVhnRuJkQtdGcwtmZjolNZN1RrJ0Ypwd=97992234749',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
